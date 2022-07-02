72988ef483579b220ad5f8f48baad32061b24a6f
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-07-02 23:11:29 +0530

    Add files via upload

---
+++ lowercaseOrUppercase.c
@@ -0,0 +1,20 @@
+//program to show wheather a char is uppercase or lowercase
+#include<stdio.h>
+int main()
+{
+    char ch ;
+    printf("enter character:");
+    scanf("%c",&ch);
+
+    if ('A'<=ch && ch<='Z'){
+        printf("character is in uppercase %c",ch);
+    }
+    else if ('a' <= ch && ch <= 'z')
+    {
+        printf("character is in lowercase %c", ch);
+    }
+    else{
+        printf("character is neither a lowercase nor a uppercase %c",ch );
+    }
+    return 0 ;
+}
